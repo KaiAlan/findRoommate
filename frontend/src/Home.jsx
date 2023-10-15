d45b389a449a3b032765002a6e4ece892f63a144
Kaialan <satyajit1501@gmail.com>
2023-10-16 02:07:11 +0530

    feat: home page customization as per design

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,17 +1,55 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 
+// asset import
+import mic from './assets/mic.png';
+import search from './assets/search.png';
+import Polygon1 from './assets/Polygon1.png';
+import Polygon2 from './assets/Polygon2.png';
+
+import Navbar from './components/Navbar';
+
 
 const Home = () => {
   return (
     <div className='bg-primary h-screen'>
-      <div className='flex justify-end items-end'>
-      <Link
-      to="/login"
-      >
-<button type="button" className="text-white bg-secondary hover:bg-secondary focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-      </Link>
-      </div>
+        <Navbar />
+
+        {/* landind section */}
+        <div>
+          <div className=''>
+            <p className='mx-24 my-20 break-words font-bold text-txt_large text-8xl w-3/5'>Your Ideal Roommate is Just a Click Away...</p>
+            <div className='mx-24 w-2/5 h-16 bg-white shadow-lg rounded-full relative'>
+              <img
+                src={search}
+                alt="search"
+                className='h-14 bottom-1 absolute left-1'
+                />
+                <p className='text-secondary text-3xl absolute bottom-4 left-20'>enter your postal code</p>
+              <img
+                src={mic}
+                alt="mic"
+                className='h-12 absolute bottom-2 right-10'
+                />
+            </div>
+          </div>
+          <div className='flex m-24 relative'>
+            <img
+              src={Polygon1}
+              alt="roommate logo"
+              className='w-1/5 absolute'
+              />
+              <img
+              src={Polygon2}
+              alt="roommate logo"
+              className='w-1/5 absolute left-52'
+              />
+          </div>
+        </div>
+
+
+
+        <div></div>
     </div>
   )
 }
