884954e081f6a9abc32cf0573dcfce7f2344608f
Kaialan <satyajit1501@gmail.com>
2023-10-28 19:09:49 +0530

    scrap changes for testing porpose

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
 
 const Test = () => {
+
+  const [Email, setEmail] = useState('');
+  const [password, setPassword] = useState(null);
+
+  const handleSubmit = async() => {
+    console.log(Email + "\n" + password);
+
+    
+    await axios.post('http://localhost:8800/signup', {
+      email : Email.toString(),
+      password : password.toString()
+    })
+    .then(console.log("succes"))
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    // await axios.get('http://localhost:8800/logout',
+    // {
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //   },
+    // })
+    // .then((res) => {
+    //   console.log(res);
+    // })
+    // .catch((err) => {
+    //   console.log(err)
+    // })
+  }
   return (
     <div className='h-screen flex justify-center items-center bg-primary'>
-        <div className='h-1/4 w-1/4 bg-review_box rounded-3xl relative'>
-            <div className='flex justify-center items-center p-6 h-3/5 bg-white rounded-3xl absolute bottom-0'>
-                <p className='text-txt_large text-lg leading-tight'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore optio expedita enim hic facilis voluptatibus magni minus nesciunt praesentium ullam! Suscipit possimus, enim eius recusandae illum ut dignissimos vitae aliquid quod, at iure alias commodi.</p>
-            </div>
-        </div>
+      <div className='flex-col items-center h-1/2 w-1/4 p-10 rounded-lg bg-txt_large'>
+              <input
+                type="text"
+                placeholder='example@gmail.com'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder='password'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+          <button
+            type='submit'
+            onClick={handleSubmit}
+            className='h-8 w-16 rounded-lg bg-green-400 font-bold'
+          > Login </button>
+
+      </div>
     </div>
   )
 }
